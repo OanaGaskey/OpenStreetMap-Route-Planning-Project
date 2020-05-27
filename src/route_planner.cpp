3527a8b66f03d7d5209983ea44fef22769a3002b
Oana <oana.gaskey@gmail.com>
2020-05-27 16:44:14 -0400

    implemented heuristic

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -21,8 +21,8 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - You can use the distance to the end_node for the h value.
 // - Node objects have a distance method to determine the distance to another node.
 
-float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-
+float RoutePlanner::CalculateHValue(RouteModel::Node const *node) { 
+  return node->distance(*end_node);
 }
 
 
